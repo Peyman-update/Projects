e8e090f3ad397037cf0a29eac62a3b3ebf98f69b
peyman <khezli1999@gmail.com>
2022-08-11 17:32:02 +0430

    Add file

---
+++ VAC.js
@@ -0,0 +1,33 @@
+
+//  let i = 1;
+
+//  let x = prompt('Enter A Number');
+
+// // while (i <= 1000) {
+
+// //     if (i == x) {
+       
+// //        document.writeln( `<h2>${i}</h2>`)
+// //         i++
+// //         continue;
+        
+// //     }
+// //     document.writeln(`${i},`);
+// //     i++;
+// // }
+
+
+// do{
+
+//     if (i == x) {
+       
+//        document.writeln( `<h2 class>${i}</h2>`)
+//         i++
+//         continue;
+//     }
+
+//     document.writeln(`${i},`);
+//      i++;
+// }
+// while (i<1000);
+
