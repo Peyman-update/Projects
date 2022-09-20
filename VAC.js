65820192f3d53e1da60ed8578578cf897954487d
peyman <khezli1999@gmail.com>
2022-09-20 23:06:54 +0330

    Method String part 2

--- VAC.js
+++ VAC.js
@@ -1,59 +1,84 @@
 
 ////////////////////// String Method ////////////////////////
 
-// charAt()	Returns the character at a specified index (position)
+// fromCharCode()	Returns Unicode values as characters , The syntax is always String.fromCharCode().
 
-// charCodeAt()	Returns the Unicode of the character at a specified index
+// includes()	Returns if a string contains a specified value .
 
-// charCodeAt() vs codePointAt()
+// repeat()	Returns a new string with a number of copies of a string , method does not change the original string.
 
-// The concat() method joins two or more strings.
+// replace()	Searches a string for a value, or a regular expression, and returns a string where the values are replaced ,method does not change the original string.
 
-// endsWith()	Returns if a string ends with a specified value
+// search()	Searches a string for a value, or regular expression, and returns the index (position) of the match
+
+// slice()	Extracts a part of a string and returns a new string , method does not change the original string.
+
+// split()	Splits a string into an array of substrings  , method does not change the original string.
+
+// startsWith()	Checks whether a string begins with specified characters , method does not change the original string.
+
+// substr()	Extracts a number of characters from a string, from a start index (position) ,method does not change the original string.
+
+// toLocaleLowerCase()	Returns a string converted to lowercase letters, using the host's locale
+
+// toLocaleUpperCase()	Returns a string converted to uppercase letters, using the host's locale
+
+// trim()	Returns a string with removed whitespaces
+
+const STR = 'fromCharCode()	Returns Unicode values as characters'
+
+const fromcharcode =String.fromCharCode(52, 21, 112, 100, 40000),
+    endwith = STR.endsWith('characters'),
+    codepointAt = STR.codePointAt(10);
+
+console.log(`fromcharcode : ${fromcharcode} \n is String above endWith (characters) ? : ${endwith}
+\n unicode index 10 as String above is : ${codepointAt} ; 
+`)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const STR2 = 'Returns if a string contains a specified value .';
+
+const include = STR2.includes('if'),
+    include2 = STR2.includes('contains', 20),
+    concatstring = STR.concat(STR2),
+    copies = STR2.repeat(3),
+    replace = STR.replace('characters', 'String'),
+    replaceFunction = STR.replace(/Returns|as|fromCharCode()|values|characters|Unicode/gi , x=> x.length);
+
+console.log(` is include ? : ${include} 
+\n is after index 20 value (contains) ? : ${include2}
+\n Two String above is concat : ${concatstring}
+\n repeat of string : ${copies}
+\n replace in The string : ${replace}
+\n replace in The function : ${replaceFunction}`);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const str = 'Extracts a part of a string and returns a new string';
+const space = 'My name is Peyman ';
+
+const slice = str.slice(0, 20),
+    sliceNagative = str.slice(-30),
+    slipt = str.split("", 4),
+    startWith = str.startsWith('Extracts'),
+    substr = str.substr(1, 15),
+    touppercase = STR.toLocaleUpperCase(),
+    tolowercase = STR.toLocaleLowerCase();
+
+console.log(`\nExtracts a part of a string : ${slice} \nA negative number selects from the end of the string : 
+${sliceNagative} \n split of string : ${slipt} \n does string start with value (Extract) ? : ${startWith} \n  Extracts a part of a string (start : 1 and end : 15) : ${substr}\n Uppercase The String : ${touppercase}\n
+LowerCase The String : ${tolowercase}\n remove space with Trim method : ${space.trim()} `);
 
 
 
 
 
-let str = 'Returns the character at a specified index (position)';
 
 
-const frist_charcter = str.charAt(12), frist_unicode = str.charCodeAt(12);
-
-console.log(`frist_unicode : ${frist_unicode} and frist_character ${frist_charcter}`);
 
 
-const last_charcter = str.charAt(str.length - 1), last_unicode = str.charCodeAt(str.length - 1);
-
-console.log(`unicode : ${last_unicode} and lastCharacter : ${last_charcter}`);
-
-
-let str1 = 'Returns the Unicode of the character at a specified index';
-
-
-const character_unicode = str1.charAt(12), unicode = str1.charCodeAt(12), unicode_point = str1.codePointAt(12);
-
-console.log(`unicode : ${unicode} and unicode_point : ${unicode_point} and character : ${character_unicode} `);
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-let concatStr = str.concat(str1);
-
-let concatindex = str.concat(str1[str1.length - 1]);
-
-console.log(`\nTwo String above are concat : ${concatStr}`);
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-let endswith = 'Returns if a string ends with a specified value';
-
-console.log(`\n is it endWith String value ? : ${endswith.endsWith('value')}`);
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
